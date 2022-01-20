67cf1be6f062b4c3ba0e7c6950059d1e0b55056c
Marion Michielsen <91435399+MarionMichielsen@users.noreply.github.com>
2022-01-20 15:29:32 +0100

    add pom filerna

--- JavaBGruppOskarMarion/JavaBGruppOskarMarion-domain/src/main/java/JavaBGruppOskarMarion/domain/Owl.java
+++ JavaBGruppOskarMarion/JavaBGruppOskarMarion-domain/src/main/java/JavaBGruppOskarMarion/domain/Owl.java
@@ -3,38 +3,26 @@ package JavaBGruppOskarMarion.domain;
 import java.util.Random;
 
 public class Owl extends Animal{
-   // private int age;
+
     private String latinName;
-   // private boolean hungry;
+
     private int wingSpan;
     private int heightAboveGround;
-  //  private int weight;
-/*
-    public int getWeight() {
-        return weight;
-    }
 
-    public void setWeight(int weight) {
-        this.weight = weight;
-    }*/
+
 
     /*public Owl(int age, int weight, int wingSpan, int heightAboveGround, boolean hungry) {
         super(weight, age, hungry);
-       // setAge(age);
         setWingSpan(wingSpan);
         setHeightAboveGround(heightAboveGround);
-      //  setHungry(hungry);
-      //  setWeight(weight);
+
     }*/
 
     public Owl(String latinName, int wingSpan, int heightAboveGround) {
         super()
 
     }
-/*
-    public void setAge(int age) {
-        this.age = age;
-    }*/
+
 
     public void setWingSpan(int wingSpan) {
         this.wingSpan = wingSpan;
@@ -46,10 +34,7 @@ public class Owl extends Animal{
 
     public void setLatinName(String latinName) {
         this.latinName = latinName;
-    }/*
-    public void setHungry(boolean hungry) {
-        this.hungry = hungry;
-    }*/
+    }
 
 
 
